d186f6cfc1d95bf1732f911ebc581d1c8907d77a
Tomek Kaczmarzyk <tspamm3r@gmail.com>
2023-01-18 00:32:34 +0100

    18.01.23 goodbye

--- main.go
+++ main.go
@@ -11,8 +11,10 @@ import (
 func main() {
 	l := log.New(os.Stdout, "product-api", log.LstdFlags)
 	hh := handlers.NewHello(l)
+	hg := handlers.NewGoodbye(l)
+	sm := http.NewServeMux()
+	sm.Handle("/", hh)
+	sm.Handle("/goodbye", hg)
 
-	http.HandleFunc("/", hh.ServeHTTP)
-
-	http.ListenAndServe(":9090", nil)
+	http.ListenAndServe(":9090", sm)
 }
